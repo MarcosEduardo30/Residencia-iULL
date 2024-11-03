e6336b2246e6f48c53c04b7ada8ac4e74cd1de63
MarcosEduardo30 <marcos302k02@gmail.com>
2024-11-03 17:40:39 -0300

    Commit inicial

---
+++ Desafio - 1.2/ConsultorioOdontologico/ConsultorioOdontologico/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsultorioOdontologico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
